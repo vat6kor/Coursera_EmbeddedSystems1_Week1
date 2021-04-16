26a76ba9d46060b69d9f250e5d18d75c93bdf5a8
Vinay Sathyanarayana <vinaysprings@gmail.com>
2021-04-16 02:10:50 +0200

    code optimization

--- stats.c
+++ stats.c
@@ -52,10 +52,10 @@ void print_statistics(uint8_t* data, uint8_t size){
 	sort_array(data, size);
 
 	printf("\nThe summary of statistics for the given dataset is as follows.\
-			\nMinimum value=%d\
-			\nMaximum value=%d\
-			\nmedian value=%d\
-			\nmean value=%d\n",data[size-1],data[0],find_median(data,size),find_mean(data,size));
+			\nMinimum value=%u\
+			\nMaximum value=%u\
+			\nmedian value=%u\
+			\nmean value=%u\n",data[size-1],data[0],find_median(data,size),find_mean(data,size));
 }
 
 void print_array(uint8_t* data, uint8_t size){
@@ -63,24 +63,17 @@ void print_array(uint8_t* data, uint8_t size){
 	printf("\nThe elements in the array are:[");
 
 	for(uint8_t i=0;i<size-1;++i){
-		printf("%d, ",data[i]);
+		printf("%u, ",data[i]);
 	}
 
-	printf("%d]\n",data[size-1]);
+	printf("%u]\n",data[size-1]);
 }
 
 uint8_t find_median(uint8_t* data, uint8_t size){
 
 	sort_array(data, size);
 
-	uint8_t median = 0;
-
-	if(size%2==0)
-		median = (data[size/2]+data[(size-1)/2])/2;
-	else
-		median = data[size/2+1];
-
-	return median;
+	return size%2==0?(data[size/2]+data[(size-1)/2])/2:data[size/2];
 
 }
 
@@ -97,17 +90,29 @@ uint8_t find_mean(uint8_t* data, uint8_t size){
 
 uint8_t find_maximum(uint8_t* data, uint8_t size){
 
-	sort_array(data, size);
+	uint8_t max = data[0];
+  
+  	for(int i=1;i<size;++i){
+    	if(data[i]>max){
+      	max = data[i];
+    	}
+  	}
 
-	return data[0];
+  return max;
 
 }
 
 uint8_t find_minimum(uint8_t* data, uint8_t size){
 
-	sort_array(data, size);
+	uint8_t min = data[0];
+  
+  	for(int i=1;i<size;++i){
+    	if(data[i]<min){
+      	min = data[i];
+    	}
+  	}
 
-	return data[size-1];
+  return min;
 
 }
 
@@ -117,15 +122,11 @@ void sort_array(uint8_t* data, uint8_t size){
 	uint8_t temp = 0;
 
 	for(uint8_t i=0;i<size;++i){
-
 		for(uint8_t j=0;j<size-i-1;++j){
-
 			if(data[j]<data[j+1]){
-
 				temp = data[j];
 				data[j] = data[j+1];
 				data[j+1]=temp;
-
 			}
 		}
 	}
